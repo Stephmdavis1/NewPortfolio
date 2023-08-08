037a49f5c0af5d7dcf11231f886915684eb56d57
Stephmdavis1 <125842540+Stephmdavis1@users.noreply.github.com>
2023-08-08 19:52:21 -0400

    updated about page text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe00c2c42eb747ef"; }
+/******/ 		__webpack_require__.h = function() { return "6dbadffe19512fbe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
